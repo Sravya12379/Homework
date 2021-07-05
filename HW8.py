95166bb6e5e73e04024d25277b0b8c0d9dcde28a
Sravya1 <sravyabetina997@gmail.com>
2021-07-05 17:54:23 +0100

    HW 8

---
+++ HW8.py
@@ -0,0 +1,30 @@
+pincode=(input("please enter valid pin"))
+n=1
+len=len(pincode)
+print(len)
+if len==4:
+   print("pin validated")
+else:
+    while n<3:
+       print("pin incorrect")
+       pincode = input("please enter valid pin")
+       len = len(pincode)
+       print(len)
+       if len == 4:
+           print("pin validated")
+
+       n=n+1
+       exit()
+bal=100
+wa=int(input("please enter withdrawl amount"))
+try:
+    if bal>wa:
+       new_bal=bal-wa
+       print("new balance="+str(new_bal))
+except:
+    print("check balance amount")
+    exit()
+
+
+
+
